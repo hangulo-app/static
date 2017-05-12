258d8739274b39bedcd2db50c6d17ca60fb5f1a3
joeyoo <joseph_yoo@brown.edu>
2017-05-12 16:56:52 -0400

    get it to work

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,79 +1,18 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition,
-    SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList,
-    SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-var gameStarted = false,
-    gameCompleted = false,
-    gameInProgress = false,
-    blocks = [],
-    currentBlock = '',
-    blockIndex = 0,
-    blinkInterval;
-
-/*
-* BEGIN SPEECH RECOGNITION
-*/
-blocks = ['안','녕','하','세','요'];
-var grammar = '#JSGF V1.0; grammar blocks; public <block> = ' + blocks.join(' | ') + ' ;'
-
-var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
-
-speechRecognitionList.addFromString(grammar, 1);
-
-recognition.grammars = speechRecognitionList;
-recognition.continuous = true;
-recognition.lang = 'ko-KR';
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
-
-recognition.onsoundend = function() {}
-
-recognition.onspeechend = function() {}
-
-recognition.onaudioend = function() {}
-
-recognition.onresult = function(event) {
-  var last = event.results.length - 1;
-  var result = event.results[last][0].transcript;
-
-  $('#block-'+blockIndex+'-guess').html(result);
-
-console.log('Confidence: ' + event.results[0][0].confidence);
-
-  if (isMatch(currentBlock, result)) {
-    // move to next
-    blockIndex ++;
-    currentBlock = blocks[blockIndex];
-
-    // ends game if last correct match is last remaining block
-    if (!currentBlock) return endGame();
-
-    setCurrentBlock(blockIndex);
-  }
-}
-
-
-// NO MATCH / ERROR HANDLERS
-  recognition.onnomatch = function(event) {
-    $('.output').html("NO MATCH FOUND");
-  }
-
-  recognition.onerror = function(event) {
-    $('.output').html('Error occurred in recognition: ' + event.error);
-  }
-
-/*
-* END SPEECH RECOGNITION
-*/
 $(document).ready(function(){
   UIkit.modal.dialog("<button class='uk-modal-close-default' type='button' uk-close></button><div class='uk-modal-header'><h2 class='uk-modal-title'>Welcome to Hangulo!</h2><div><p>Hangulo is currently under development and not yet functional. Feel free to try the demo on this page, nonetheless.</p><p>Any questions can be mailed to <span class='uk-text-primary'>joseph@hangulo.org</span></p>", {center:true});
+});
+
+
+$('#playButton').on('click', function(e) {
+  e.preventDefault();
+
+  if (!Demo.inProgress) startGame();
 });
 
 function setCurrentBlock(blockId) {
   resetBlocks();
 
-  $currentBlock = $('#word-list > #block-'+blockId+'');
+  $currentBlock = $('#word-list > #block-'+blockId);
 
   $currentBlock.addClass('uk-active');
   $currentBlock.children('h3').addClass('uk-heading-primary');
@@ -84,42 +23,6 @@ function resetBlocks() {
 
   $allBlocks.removeClass('uk-active');
   $allBlocks.children('h3').removeClass('uk-heading-primary');
-};
-
-$('#playButton').on('click', function(e) {
-  e.preventDefault();
-
-  if (!gameInProgress) startGame();
-});
-
-function startGame() {
-  recognition.start();
-};
-  recognition.onstart = function() {
-    gameStarted = true;
-    gameInProgress = true;
-    gameCompleted = false;
-    blockIndex = 0;
-
-    startBlinking();
-    setCurrentBlock(blockIndex);
-  };
-
-function endGame() {
-  recognition.stop();
-};
-  recognition.onend = function() {
-    gameStarted = false;
-    gameInProgress = false;
-    gameCompleted = true;
-    blockIndex = 0;
-
-    stopBlinking(blinkInterval);
-    resetBlocks();
-  };
-
-function isMatch(block, guess) {
-  return block == guess;
 };
 
 function blink() {
---
+++ assets/js/speechRecog.js
@@ -0,0 +1,156 @@
+/*
+**** GLOBAL VARS
+*/
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition,
+    SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList,
+    SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent,
+
+    recognition,
+    recognitionList,
+    blocks = ['안','녕','하','세','요'];
+
+// GAME STATE
+var Demo = {
+  started: false,
+  completed: false,
+  inProgress: false,
+  currentBlock: '',
+  blockIndex: 0,
+  blinkInterval: null
+};
+/*
+****
+*/
+
+/*
+**** Recognition Config
+*/
+  recognition = new SpeechRecognition();
+  recognitionList = new SpeechGrammarList();
+
+  recognitionList.addFromString('#JSGF V1.0; grammar blocks; public <block> = ' + blocks.join(' | ') + ' ;');
+  // recognition config
+  recognition.grammars = recognitionList;
+  recognition.lang = 'ko-KR';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+/*
+****
+*/
+
+/*
+**** EVENT HANDLERS
+*/
+
+// Unsuccessful result handlers
+  recognition.onnomatch = function(event) { alert(event) };
+  recognition.onerror = function(event) { console.log(event) };
+//
+
+// when recording begins for each block
+  recognition.onstart = function() {
+
+    startBlinking();
+    setCurrentBlock(Demo.blockIndex);
+  }
+  recognition.onend = function() {
+    stopBlinking(Demo.blinkInterval);
+
+    if (Demo.inProgress) continueGame();
+  }
+
+  recognition.onresult = function(event) {
+    var last = event.results.length - 1;
+    var result = event.results[last][0].transcript;
+    var block = Demo.currentBlock;
+
+console.log("charAt(0) = ",result.charAt(0));
+console.log("charAt(result.length-1) = ", result.charAt(result.length-1));
+
+    if (result.length-1 > 0 && result.length > block.length) {
+      if (block == result.charAt(0)) {
+        alert(result);
+      }
+      else if (block == result.charAt(result.length-1)) {
+
+console.log("isMultiMatchBackward: ", isMultiMatchBackward(block, result));
+
+        if (isMultiMatchBackward(block, result)) {
+
+          $('#block-'+Demo.blockIndex+'-guess').html(result.charAt(result.length-1));
+
+          Demo.blockIndex++;
+          Demo.currentBlock = blocks[Demo.blockIndex];
+        }
+      };
+    }
+    else {
+      $('#block-'+Demo.blockIndex+'-guess').html(result);
+
+      if (isMatch(Demo.currentBlock, result)) {
+        Demo.blockIndex++;
+        Demo.currentBlock = blocks[Demo.blockIndex];
+
+        if(!Demo.currentBlock) endGame();
+      }
+    };
+
+  }
+/* END EVENT HANDLERS */
+
+function startGame() {
+  Demo.started = true;
+  Demo.inProgress = true;
+  Demo.completed = false;
+  Demo.blockIndex = 0;
+  Demo.currentBlock = blocks[Demo.blockIndex];
+
+  recognition.start();
+};
+
+function continueGame() {
+  setCurrentBlock(Demo.blockIndex);
+
+  recognition.start();
+};
+
+function endGame() {
+  recognition.abort();
+
+  Demo.started = false;
+  Demo.inProgress = false;
+  Demo.completed = true;
+  Demo.blockIndex = 0;
+  Demo.currentBlock = blocks[Demo.blockIndex];
+
+  resetBlocks();
+};
+
+function isMatch(block, guess) {
+  return block == guess;
+};
+
+function isMultiMatchForward(block, guess) {
+  var refIndex = blocks.indexOf(block);
+  var multiCharBlockForward = '';
+
+  for (var i=refIndex;i<blocks.length;i++){ //forward
+    if (guess == multiCharBlockForward) return true;
+
+    multiCharBlock += blocks[i];
+  };
+  return false;
+};
+
+function isMultiMatchBackward(block, guess) {
+  var refIndex = blocks.indexOf(block);
+  var multiCharBlockBackward = '';
+
+  for (var i=refIndex;i>-1;i--){ //backward
+    multiCharBlockBackward = blocks[i] + multiCharBlockBackward;
+console.log("guess: ", guess);
+console.log("multiCharBlockBackward: ", multiCharBlockBackward);
+    if (guess == multiCharBlockBackward) return true;
+  };
+  return false;
+};
